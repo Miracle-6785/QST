9f1488b0306b426d481c9616bcc4afd045050d78
BuiXuanSon1412 <bxson1412@gmail.com>
2024-06-10 23:47:15 +0700

    label data

---
+++ data/plain2json.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+input_files = ["plain/positive.txt",
+               "plain/negative.txt",
+               "plain/neutral.txt"]
+
+output_files = ["json/positive.json",
+                "json/negative.json",
+                "json/neutral.json"]
+
+label_map = {"positive.txt": 'positive', "negative.txt": 'negative', "neutral.txt": 'neutral'}
+for i, input_file in enumerate(input_files):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    data = {'data': []}
+    for line in lines:
+        line = line.strip()
+        if line:
+            data['data'].append({'text': line, 'label': label_map[os.path.basename(input_file)]})
+
+    with open(output_files[i], 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
